d9128c2ac120f5a5c96ad36fa211abfefa7cff57
cokeefe <chaemobile@gmail.com>
2013-04-05 14:01:22 -0400

    Euler 4

---
+++ 2013_March.rb
@@ -0,0 +1,24 @@
+=begin 
+Rails Girls Altanta 
+March 2013 Project
+Euler Problem 4
+A palindormic number reads the same both ways. The largest palindrome made 
+from the product of two 2-digit numbers is 9009, which is 91 times 99 .
+Find the largest palindrome made from the product of two 3-digit numbers.
+=end
+
+sum = []
+
+(100..999).map do |x|
+        (100..999).map do |y|
+         sum << x*y
+       end
+     end
+     
+pal = []
+
+sum.flatten.select do |x|
+   pal << x if x.to_s == x.to_s.reverse
+end
+
+puts pal.sort.last
