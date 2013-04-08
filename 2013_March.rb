477bba995e81e2ef2bb4550d31d6c51b231c0a1a
cokeefe <chaemobile@gmail.com>
2013-04-08 08:09:36 -0400

    updated note

--- 2013_March.rb
+++ 2013_March.rb
@@ -1,4 +1,5 @@
 =begin 
+
 Rails Girls Altanta 
 March 2013 Project
 Euler Problem 4
@@ -6,6 +7,7 @@ A palindormic number reads the same both ways. The largest palindrome made
 from the product of two 2-digit numbers is 9009, which is 91 times 99 .
 Find the largest palindrome made from the product of two 3-digit numbers.
 =end
+
 
 sum = []
 
@@ -22,3 +24,10 @@ sum.flatten.select do |x|
 end
 
 puts pal.sort.last
+
+
+# James Holder pointed out to me though, that the second part could be refractored to
+
+#   pal = sum.flatten.select do |x|
+#         x if x.to_s = x.to_s.reverse
+# =>      end
